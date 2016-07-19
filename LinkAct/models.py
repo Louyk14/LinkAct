e7a79ab93312b72d09ed6dd40fc4e511e2fdcc98
Louyk14 <louyk14@163.com>
2016-07-19 18:02:54 +0800

    Update models.py

--- LinkAct/models.py
+++ LinkAct/models.py
@@ -4,3 +4,26 @@ from django.db import models
 #show what front page need
 class Front(models.Model):
 	a = 'aaaa'
+	
+from django import forms
+from .models import MyUser
+from .models import activity
+from .models import interest
+
+#注册信息
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = MyUser
+        fields = ('uername', '')
+
+#创建活动信息
+class ActForm(forms.ModelForm):
+    class Meta:
+        model = activity
+        fields = ('locale', 'theme', 'start_date', 'end_data', 'introduction')
+
+#发起评论信息
+class CommentForm(form.ModelForm):
+    class Meta:
+        model = interest
+        fields = ('content', 'id')
