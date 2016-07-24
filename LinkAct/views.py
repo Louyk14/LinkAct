8c4088722ab8849ccd8dd78b489d6b688cd93157
Lcorvle <xsx1996@163.com>
2016-07-24 23:55:05 +0800

    Update views.py

--- LinkAct/views.py
+++ LinkAct/views.py
@@ -17,8 +17,12 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from LinkAct.models import Img, Interest
 import string
+from datetime import date
+from .forms import ActForm
+from django.utils import timezone
 
 base_url = 'http://127.0.0.1:8000'
+
 
 
 #   search_class表示搜索类别，search_content表示搜索内容,search_content表示搜索的页码号，要在template中动态生成
@@ -258,13 +262,13 @@ def user_register(request):
 			#信息不完整
 			print('incomplete info')
 			return render(request, 'LinkAct/result_page.html', {'error_index':3})
+		if password1 != password2:
+			print('password1 is not equal to password2')
+			return render(request, 'LinkAct/result_page.html', {'error_index':1})
 		if len(User.objects.filter(username=usernames)):
 			#用户名已存在
 			print('username already exists')
 			return render(request, 'LinkAct/result_page.html', {'error_index':2})
-		if password1 != password2:
-			print('password1 is not equal to password2')
-			return render(request, 'LinkAct/result_page.html', {'error_index':1})
 		#判定完毕
 		myUser = MyUser()
 		myUser.create_user(usernames, password1, email, nickname, birthday, city, interests)
@@ -277,81 +281,71 @@ def user_register(request):
 
 
 #创建完成
-def over_create_act(request):
-	#在全局绑定函数中判断按下了哪个按钮，此处需知道当前用户名，默认活动form为ActForm
-	if request.method == 'POST':
-		params = request.POST
-		form = ActForm()
-		form.status = params.get('status', '')
-		form.creator = request.user.id
-		form.locale = params.get('locale', '')
-		form.theme = params.get('theme', '')
-		form.create_date = params.get('create_date', '')
-		form.start_date = params.get('start_date', '')
-		form.end_date = params.get('end_date', '')
-		form.introduction = params.get('introduction', '')
-		form.save()
-		 
-		return HttpResponseRedirect('search/?search_class=create_time&search_content=None&search_page=1&search_order=1')
-	else:
-		form = ActForm()
-		return render(request, 'msgboard/createAct.html')
+#创建完成
+def create_act(request):
+    #在全局绑定函数中判断按下了哪个按钮，此处需知道当前用户名，默认活动form为ActForm
+    if request.method == 'POST':
+        params = request.POST
+        new_act = Activity()
+        new_act.status = '0'
+        new_act.creator = request.user.id
+        new_act.locale = params.get('locale', '')
+        new_act.theme = params.getlist('theme', '')
+        new_act.create_date = timezone.now()
+        new_act.start_date = params.get('start_date', '')
+        new_act.end_date = params.get('end_date', '')
+        new_act.introduction = params.get('introduction', '')
+        new_act.save()
+         
+        return HttpResponseRedirect('../search/?search_class=create_time&search_content=None&search_order=1&search_page=1')
+    else:
+        form = ActForm()
+        return render(request, 'msgboard/createAct.html', {form, 'form'})
 
 #参加活动，传入活动id，如何根据request获取当前用户id，此处还未判断是否人满
+#参加活动，传入活动id，如何根据request获取当前用户id，此处还未判断是否人满
 def enter_act(request):
-	if request.method == 'GET':
-		i = request.GET['id']
-		to_enter_act = Activity.objects.get(id=i)
-		to_enter_act.append_participants(request.user.id)
-		request.user.myuser.append_participate_ongoing_acts(to_enter_act.id)
+    if request.method == 'GET':
+        i = request.GET['id']
+        to_enter_act = Activity.objects.get(id=i)
+        to_enter_act.append_participants(request.user.id)
+        request.user.myuser.append_participate_ongoing_acts(to_enter_act.id)
 
-		return render(requset, '同上', {'无参数'})
+        return render(requset, '同上', {'无参数'})
 
 #退出活动
 def exit_act(request):
-	if request.method == 'GET':
-		i = request.GET['id']
-		to_exit_act = Activity.objects.get(id=i)
-		to_exit_act.remove_participants(request.user.id)
-		request.user.myuser.remove_participate_ongoing_acts(to_enter_act.id)
+    if request.method == 'GET':
+        i = request.GET['id']
+        to_exit_act = Activity.objects.get(id=i)
+        to_exit_act.remove_participants(request.user.id)
+        request.user.myuser.remove_participate_ongoing_acts(to_enter_act.id)
 
-		#从用户正参加列表中删除
-		return render(requset, '同上', {'无参数'})
+        #从用户正参加列表中删除
+        return render(requset, '同上', {'无参数'})
 
-#查看活动信息——id写在网址上，通过链接获得，<a href="">
+#修改活动信息，仅活动创建人能进入此页面，修改完成后用input按钮提交，用hidden的input标签传回id及last_page
 def check_act_msg(request):
-	if request.method == 'GET':
-		is_in_act = False
-		i = request.GET['id']
-		to_check_act = Activity.objects.get(id=i)
+    if request.method == 'GET':
+        i = request.GET['id']
+        last_page = request.GET['last_page']
+        to_check_act = Activity.objects.get(id=i)
+        
+        form = ActForm()
+        
+        return render(request, 'msgboard/actShow.html', {'form': form, 'act_obj':to_check_act, 'last_page':last_page, 'id':i})
+    else:
+        params = request.POST
+        i = request.POST['id']
+        last_page = request.POST['last_page']
+        to_check_act = Activity.objects.get(id=i)
+        to_check_act.set_locale(params.get('locale', ''))
+        to_check_act.set_theme(params.get('theme', ''))
+        to_check_act.update_start_date(params.get('start_date', ''))
+        to_check_act.update_end_date(params.get('end_date', ''))
+        to_check_act.set_introduction(params.get('introduction', ''))
 
-		if request.user.id in to_check_act.get_participants:
-			is_in_act = True
-		
-		form = ActForm()
-		form.status = to_check_act.get_status()
-		form.creator = to_check_act.get_creator()
-		form.locale = to_check_act.get_locale()
-		form.theme = to_check_act.get_theme()
-		form.create_date = to_check_act.get_create_date()
-		form.start_date = to_check_act.get_start_date()
-		form.end_date = to_check_act.get_end_date()
-		form.introduction = to_check_act.get_introduction()
-		
-		return render(request, 'msgboard/actShow.html', {'form': form, 'is_in_act':is_in_act})
-	else:
-		params = request.POST
-		i = request.GET['id']
-		to_check_act = Activity.objects.get(id=i)
-		to_check_act.set_status(params.get('status', ''))
-		to_check_act.set_creator(params.get('creator', ''))
-		to_check_act.set_locale(params.get('locale', ''))
-		to_check_act.set_theme(params.get('theme', ''))
-		to_check_act.update_start_date(params.get('start_date', ''))
-		to_check_act.update_end_date(params.get('end_date', ''))
-		to_check_act.set_introduction(params.get('introduction', ''))
-
-		return  #跳转页面
+        return HttpResponseRedirect(last_page)
 
 #登录
 def log_in(request):
@@ -393,6 +387,7 @@ def check_personal_msg(request):
 			print('logout successfully')
 	#-----------登录判定----------#
 
+
 	if request.method == 'POST':
 		params = request.POST
 		obj = User.objects.get(username=request.user.username)
@@ -400,7 +395,7 @@ def check_personal_msg(request):
 		obj.myuser.set_nickname(params.get('nickname', ''))
 		#obj.myuser.set_birthday(params.get('birthday', ''))
 		obj.myuser.set_city(params.get('city', ''))
-		
+
 		imgs = Img.objects.filter(id = obj.myuser.get_head())
 		if len(imgs) != 0:
 			img = imgs[0]
@@ -411,13 +406,13 @@ def check_personal_msg(request):
 																'error_index':9, 'img': img})
 
 	#default render#
-	else :        
+	else :       
 		imgs = Img.objects.filter(id = request.user.myuser.get_head())
 		if len(imgs) != 0:
 			img = imgs[0]
 		else:
 			img = Img.objects.all()[0]
-			
+
 		form = PersonalInfoForm()
 		form.email = request.user.email
 		form.nickname = request.user.myuser.nickname
@@ -512,25 +507,49 @@ def evaluate_act(request):
 		itemID = params['id']
 		return render(request, '任务信息界面', {'itemID':itemID})
 		
-		
-def search_people(request):
-	if request.method == 'GET':                                  
-		search_class = request.GET['search_class']
-		search_content = request.GET['search_content']                                  
-		search_page = request.GET['search_page']
-		search_order = request.GET['search_order']
-		
-		if search_order == '1':
-			answer = MyUser.objects.order()
-		
-		elif search_class == 'nickname':
-			answer = MyUser.objects.filter(nickname=search_content)
+def show_people(request):
+    if request.method == 'GET':
+        index = int(request.GET['id'])
+        current_page = request.GET['last_page']
+        obj = MyUser.objects.filter(id=index)
 
-		startPos = (int(search_page) - 1) * 10
-		endPos = int(search_page) * 10
-		result = answer[startPos:endPos]
-		return render(request, 'msgboard/explore_page.html', {'answer':answer})    
-		
+        new_obj = []
+        for index in range(0, len(obj)):
+            if obj[index].user.username != request.user.username:
+                new_obj.append(obj)
+
+        return render(request, 'LinkAct/showPeople.html', {'form':form, 'obj':new_obj, 'last_page':current_page})
+
+def search_people(request):
+    if request.method == 'GET':                                  
+        search_class = request.GET['search_class']
+        search_content = request.GET['search_content']                                  
+        search_page = request.GET['search_page']
+        search_order = request.GET['search_order']
+        
+        if search_order == '1':
+            answer = MyUser.objects.all().order_by('id')
+        
+        elif search_class == 'nickname':
+            answer = MyUser.objects.filter(nickname=search_content)
+
+        
+        startPos = (int(search_page) - 1) * 10
+        endPos = int(search_page) * 10
+        if endPos >= len(answer):
+            endPos = len(answer)
+
+        result = answer[startPos:endPos]
+
+        temp_url = request.get_full_path()
+
+        next_page = int(search_page) + 1
+        
+        next_page_url = request.path + "?search_class=" + search_class + "&search_content=" + search_content + "&search_order=" + search_order + "&search_page=" + str(next_page)
+        
+        return render(request, 'msgboard/show_people.html', {'result':result, 'current_page':int(search_page), 'current_url':request.get_full_path(), 'next_page_url':next_page_url})    
+
+	
 #查找活动   //搜索页面不同于主页面 默认每页10条，所有展示活动的界面都绑定此函数
                 #urls.py r'^searchActs/'    绝对路径为127.0.0.0.1/searchActs/      因而展示界面上的各详情链接为  <a href='../showActs/?id='+ {{ result[index].id }} + '&last_page=' + {{ current_url }}>
                 #“下一页”按钮链接应为<a href={{ next_page_url }}>
@@ -553,23 +572,32 @@ def search_act(request):
             answer = Activity.objects.all()[0].activity_theme_filter(Activity.objects.all(), [search_class])
 
         elif search_class == '':
-
+            return
+        
         startPos = (int(search_page) - 1) * 10
         endPos = int(search_page) * 10
+        if endPos >= len(answer):
+            endPos = len(answer)
+        
         result = answer[startPos:endPos]
+
+        next_page = int(search_page) + 1
 
         temp_url = request.get_full_path()
         next_page_url = request.path + "?search_class=" + search_class + "&search_content=" + search_content + "&search_order=" + search_order + "&search_page=" + (int(search_page) + 1)
 
-        return render(request, 'msgboard/searchActs.html', {'result':result, 'current_page':int(search_page)}, 'current_url':request.get_full_path(), 'next_page_url':next_page_url})
+        return render(request, 'msgboard/searchActs.html', {'result':result, 'current_page':int(search_page), 'current_url':request.get_full_path(), 'next_page_url':next_page_url})
 
 
-#展示具体活动的界面，返回按钮的链接应为<a href={{ last_page }}>
+#展示具体活动的界面，返回按钮的链接应为<a href={{ last_page }}>， 若是创建者，则应存在链接“修改活动信息”，应为<a href=this_page_no_para + 'change/?id=' + act_obj.id + '&last_page=' + this_page>
 def show_act(request):
     if request.method == 'GET':
         index = int(request.GET['id'])
         current_page = request.GET['last_page']
         act_obj = Activity.objects.filter(id=index)
+
+        this_page = request.get_full_path()
+        this_page_no_para = request.path
 
         actForm = ActForm()
 
@@ -578,7 +606,7 @@ def show_act(request):
         if request.user.id == act_obj.creator:
             isCreator = True
 
-        return render(request, 'LinkAct/showAct.html', {'form':actForm, 'act_obj':act_obj, 'last_page':current_page, 'isCreator':isCreator})
+        return render(request, 'LinkAct/showAct.html', {'form':actForm, 'act_obj':act_obj, 'last_page':current_page, 'this_page':this_page, 'this_page_no_para':this_page_no_para, 'isCreator':isCreator})
 
 #添加好友
 def request_for_friend(request):
